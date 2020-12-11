8b6d23164c93d12adca24c8a3e42cf5c0443d9d4
zeinab_mohamed <zeinabmohames990@gmail.com>
2020-12-11 11:36:44 +0200

    my files

---
+++ main.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int x, y;
+    cout<<"*********enter the first number        : ";
+    cin>>x;
+    cout<<endl;
+    cout<<"*********enter the second number        : ";
+    cin>>y;
+    cout <<endl;
+    cout<<"************** the mult = "<< x*y <<endl;
+    return 0;
+
+}
